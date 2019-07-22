526031fb6c2e484919ddf95948987bd6e964f7b7
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-07-22 22:43:38 +0100

    isogram: remove ignore comments from test file

--- isogram/test/test_isogram.c
+++ isogram/test/test_isogram.c
@@ -17,79 +17,66 @@ static void test_empty_string(void)
 
 static void test_null(void)
 {
-   TEST_IGNORE();               // delete this line to run test
    TEST_ASSERT_FALSE(is_isogram(NULL));
 }
 
 static void test_lower_case_only(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("isogram"));
 }
 
 static void test_duplicated_letter(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("eleven"));
 }
 
 static void test_duplicated_letter_from_end_of_alphabet(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("zzyzx"));
 }
 
 static void test_longest_known_isogram(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("subdermatoglyphic"));
 }
 
 static void test_duplicated_letter_mixed_case(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("Alphabet"));
 }
 
 static void test_duplicated_letter_mixed_case_lowercase_first(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("alphAbet"));
 }
 
 static void test_non_letter_char(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("thumbscrew-japingly"));
 }
 
 static void test_duplicated_letter_following_non_letter_char(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("thumbscrew-jappingly"));
 }
 
 static void test_duplicated_non_letter_char(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("six-year-old"));
 }
 
 static void test_multiple_whitespace(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_TRUE(is_isogram("Emily Jung Schwartzkopf"));
 }
 
 static void test_duplicated_letter_within_word(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("accentor"));
 }
 
 static void test_same_first_and_last_characters(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_FALSE(is_isogram("angola"));
 }
 
