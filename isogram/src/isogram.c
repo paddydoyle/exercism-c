019a30f74a65cee673c366065841b8183b05187f
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-07-22 22:39:41 +0100

    isogram: test for upper/lower case inputs

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -1,9 +1,11 @@
 #include "isogram.h"
+#include <ctype.h>
 #include <string.h>
 
 bool is_isogram(const char phrase[]) {
 	unsigned int i, j;
 	const char STOP_LETTERS[] = " -";
+	char c;
 
 	// Test input
 	if (phrase == NULL) {
@@ -12,11 +14,20 @@ bool is_isogram(const char phrase[]) {
 
 	// Search for a counter-example.
 	for (i=0; i<strlen(phrase); i++) {
+		c = phrase[i];
+
+		// Non-alpha character.
+		if (!isalpha(c)) {
+			return false;
+		}
+
+		c = tolower(c);
+
 		for (j=i+1; j<strlen(phrase); j++) {
 			if (strchr(STOP_LETTERS, phrase[i])) {
 				continue;
 			}
-			if (phrase[i] == phrase[j]) {
+			if (c == tolower(phrase[j])) {
 				// Counter-example found.
 				return false;
 			}
