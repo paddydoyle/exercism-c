cfa4a248c4fdba31ab67b4cfda9ab4cdc9e76b62
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-08-06 22:52:19 +0100

    isogram: try O(n) version

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -2,10 +2,16 @@
 #include <ctype.h>
 #include <string.h>
 
+#define SET_BIT(mask, pos) (mask |= (1 << (pos)))
+#define GET_BIT(mask, pos) ((mask >> (pos)) & 1)
+
 bool is_isogram(const char phrase[]) {
-	unsigned int i, j;
+	unsigned int i;
 	const char STOP_CHARS[] = " -";
 	char c;
+	// Assuming int is at least 32-bit, to allow for at least
+	// 26 bit positions representing 'a' to 'z'.
+	unsigned int mask = 0;
 
 	// Test for edge case.
 	if (phrase == NULL) {
@@ -23,15 +29,13 @@ bool is_isogram(const char phrase[]) {
 
 		c = tolower(c);
 
-		for (j=i+1; j<strlen(phrase); j++) {
-			if (strchr(STOP_CHARS, phrase[j])) {
-				continue;
-			}
-			if (c == tolower(phrase[j])) {
-				// Counter-example found.
-				return false;
-			}
+		// Already in the mask?
+		if (GET_BIT(mask, c - 'a')) {
+			return false;
 		}
+
+		// Mark the char as already encountered.
+		SET_BIT(mask, c - 'a');
 	}
 
 	return true;
