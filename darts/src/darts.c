72c51207e17e5917f101e61d7b9a2ec441c94726
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-08-22 22:15:32 +0100

    darts: solution

--- darts/src/darts.c
+++ darts/src/darts.c
@@ -1,1 +1,29 @@
 #include "darts.h"
+#include <math.h>
+
+const score_map_t SCORES[4] = {
+	{10, 0},
+	{5, 1},
+	{1, 5},
+	{0, 10}
+};
+
+uint8_t score(coordinate_t landing_position) {
+	uint8_t i, n_scores;
+	double distance;
+
+	distance = sqrt(landing_position.x * landing_position.x +
+		landing_position.y * landing_position.y);
+
+	// Save re-calculating each time through the loop.
+	n_scores = sizeof(SCORES) / sizeof(score_map_t);
+
+	for (i=0; i<n_scores; i++) {
+		if (distance > SCORES[i].min_radius) {
+			return SCORES[i].score;
+		}
+	}
+
+	// Special case for dead center: the last score
+	return SCORES[n_scores-1].score;
+}
--- darts/src/darts.h
+++ darts/src/darts.h
@@ -1,4 +1,18 @@
 #ifndef DARTS_H
 #define DARTS_H
 
+#include <stdint.h>
+
+typedef struct coordinate {
+	double x, y;
+} coordinate_t;
+
+typedef struct score_map {
+	double min_radius;
+	uint8_t score;
+} score_map_t;
+
+
+uint8_t score(coordinate_t landing_position);
+
 #endif
