980576c09adf6ff3dc35dcc2e6668ec013720921
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-09-04 22:28:35 +0100

    armstrong-numbers: working

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -1,1 +1,40 @@
 #include "armstrong_numbers.h"
+#include <math.h>
+#include <stdio.h>
+
+int _countDigits(int num);
+int _countDigits(int num) {
+	int count = 0;
+
+	for (; num > 0; num /= 10) {
+		count++;
+	}
+
+	printf("_countDigits; num = %d; count = %d\n", num, count);
+
+	return count;
+}
+
+bool isArmstrongNumber(int num) {
+	int origNum = num;
+	int sum = 0;
+	int numDigits, digit;
+
+	numDigits = _countDigits(num);
+
+	printf("isArm; num = %d; count = %d\n", num, numDigits);
+
+	while (num > 0) {
+		digit = num % 10;
+		printf("isArm; digit = %d\n", digit);
+
+		sum += (int)pow(digit, numDigits);
+		printf("isArm; sum = %d\n", sum);
+
+		num /= 10;
+
+		printf("isArm; num = %d; sum = %d; digit = %d\n", num, sum, digit);
+	}
+
+	return origNum == sum;
+}
--- armstrong-numbers/src/armstrong_numbers.h
+++ armstrong-numbers/src/armstrong_numbers.h
@@ -1,4 +1,8 @@
 #ifndef ARMSTRONG_NUMBERS
 #define ARMSTRONG_NUMBERS
 
+#include <stdbool.h>
+
+bool isArmstrongNumber(int num);
+
 #endif
