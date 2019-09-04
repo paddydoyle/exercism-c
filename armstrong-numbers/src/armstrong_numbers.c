aab524c16f6a29e45b1306d77110ebe497313ef6
Paddy Doyle <paddy@tchpc.tcd.ie>
2019-09-04 22:30:57 +0100

    armstrong-numbers: tidy up debug printfs

--- armstrong-numbers/src/armstrong_numbers.c
+++ armstrong-numbers/src/armstrong_numbers.c
@@ -1,7 +1,7 @@
 #include "armstrong_numbers.h"
 #include <math.h>
-#include <stdio.h>
 
+// Helper function.
 int _countDigits(int num);
 int _countDigits(int num) {
 	int count = 0;
@@ -9,8 +9,6 @@ int _countDigits(int num) {
 	for (; num > 0; num /= 10) {
 		count++;
 	}
-
-	printf("_countDigits; num = %d; count = %d\n", num, count);
 
 	return count;
 }
@@ -22,18 +20,12 @@ bool isArmstrongNumber(int num) {
 
 	numDigits = _countDigits(num);
 
-	printf("isArm; num = %d; count = %d\n", num, numDigits);
-
 	while (num > 0) {
 		digit = num % 10;
-		printf("isArm; digit = %d\n", digit);
 
 		sum += (int)pow(digit, numDigits);
-		printf("isArm; sum = %d\n", sum);
 
 		num /= 10;
-
-		printf("isArm; num = %d; sum = %d; digit = %d\n", num, sum, digit);
 	}
 
 	return origNum == sum;
